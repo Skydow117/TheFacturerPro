3f886d9b9f7e949c6e55c7ccb538779d8f127a0c
David Castillo <davidcama15@gmail.com>
2017-02-08 18:27:42 +0100

    layout changes

--- TheFacturerPro/TheFacturerPro/Ajuda.cs
+++ TheFacturerPro/TheFacturerPro/Ajuda.cs
@@ -14,6 +14,7 @@ namespace TheFacturerPro
     {
         public Ajuda()
         {
+            StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
         }
     }
--- TheFacturerPro/TheFacturerPro/Properties/Resources.Designer.cs
+++ TheFacturerPro/TheFacturerPro/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace TheFacturerPro.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap iconeAjuda {
+            get {
+                object obj = ResourceManager.GetObject("iconeAjuda", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
